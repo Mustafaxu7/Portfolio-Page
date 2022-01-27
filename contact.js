89de94550abb82d3d322d8f50a9be47aeb8cf262
Mustafa Unal <94565017+MustafaxUK@users.noreply.github.com>
2022-01-27 23:34:48 +0000

    Contact page complete - minor adjusments could do

--- contact.js
+++ contact.js
@@ -12,3 +12,39 @@ questions.forEach((question) => {
         question.classList.toggle("show-text")
     })
 })
+
+
+const form = document.getElementById("my-form");
+    async function handleSubmit(event) {
+        event.preventDefault();
+        let status = document.getElementById("status");
+        let data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "You successfully submitted the form!";
+            form.reset()
+            status.classList.add("success")
+        } 
+        else {
+            response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } 
+            else {
+                status.innerHTML = "Oops! There was a problem submitting your form"
+                status.classList.add("error")
+            }
+            })
+        }
+        }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+        });
+    }
+    form.addEventListener("submit", handleSubmit)
+
